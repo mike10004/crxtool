cbfc4736eec252d823f6eb483ba978dae601acac
Mike Chaberski <mchaberski@gmail.com>
2018-10-10 12:03:10 -0400

    add some documentation

--- crxtool-core/src/main/java/io/github/mike10004/crxtool/Zipping.java
+++ crxtool-core/src/main/java/io/github/mike10004/crxtool/Zipping.java
@@ -6,11 +6,22 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * Static utility methods relating to zip archives.
+ *
+ * <p>This is public because it is used by the Maven plugin.
+ */
 public class Zipping {
 
     private Zipping() {}
 
-
+    /**
+     * Creates a byte array whose content is a zip archive containing all files in a directory.
+     * @param extensionDir directory
+     * @param zipConfig configuration
+     * @return the byte array
+     * @throws IOException if I/O goes awry
+     */
     public static byte[] zipDirectory(Path extensionDir, @Nullable ZipConfig zipConfig) throws IOException {
         ByteArrayOutputStream zipBuffer = new ByteArrayOutputStream(1024);
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(zipBuffer)) {
